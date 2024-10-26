d28b5db8d3d7293fa2f957732e1e764be11e450a
StefanChervenkov <stefan.chervenkov.sth@gmail.com>
2024-10-26 23:12:48 +0300

    Adding the Bus-schedule app functionality

--- 02.Bus-Schedule/app.js
+++ 02.Bus-Schedule/app.js
@@ -1,11 +1,39 @@
-function solve() {
 
+
+function solve() {
+    const departButton = document.querySelector('#depart');
+    const arriveButton = document.querySelector('#arrive');
+    const infoBoxSpanEl = document.querySelector('#info span');
+
+    let currentStopName = 'Depot' // default value, will be changed dynamically
+    let currentStopId = 'depot'; // default value, will be changed dynamically
+    let nextStopId = '';
+
+    
     function depart() {
-        console.log('Depart TODO...');
+        
+        
+        
+        // get data
+        fetch(`http://localhost:3030/jsonstore/bus/schedule/${currentStopId}`)
+        .then(response => response.json())
+        .then(data => {
+            //we change the text in the info box 
+            infoBoxSpanEl.textContent = `Next stop ${data.name}`;
+            departButton.disabled = true;
+            arriveButton.disabled = false;
+            // set the busStopId to the next stop
+            currentStopId = data.next;
+            currentStopName = data.name;
+            
+        })
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        departButton.disabled = false;
+        arriveButton.disabled = true;
+        infoBoxSpanEl.textContent = `Arriving at ${currentStopName}`
+        
     }
 
     return {
