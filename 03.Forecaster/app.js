a61f42a79abbf2877291e8e0513c17982e97c32f
StefanChervenkov <stefan.chervenkov.sth@gmail.com>
2024-10-27 12:39:49 +0200

    Adding fucntionality for initial request on 03.Forecaster app

--- 03.Forecaster/app.js
+++ 03.Forecaster/app.js
@@ -1,5 +1,44 @@
+
+
 function attachEvents() {
-    console.log("TODO...");
+    const getWeatherBtn = document.querySelector('#submit');
+    const locationInputEl = document.querySelector('#location');
+    const baseUrl = 'http://localhost:3030/jsonstore/forecaster';
+    const forecastSectionEl = document.querySelector('#forecast');
+
+    getWeatherBtn.addEventListener('click', () => {
+        const locationInput = locationInputEl.value.toLowerCase();
+
+        if (locationInput == '') {
+            return;
+        }
+        
+        let locationCode = '';
+        fetch(`${baseUrl}/locations`)
+            .then(res => res.json())
+            .then(locations => {
+                
+                locations.forEach(element => {
+                    if (element.name.toLowerCase() == locationInput) {
+                        locationCode = element.code;
+                    }
+                });
+
+                if (locationCode == '') {
+                    throw new Error("Error");
+                    
+                }
+                
+                // make new requests to collect forecast for current and upcoming conditions
+                
+            })
+            .catch((err) => {
+                forecastSectionEl.style.display = 'inline-block';
+                forecastSectionEl.textContent = err.message;
+
+            });
+
+    })
 }
 
 attachEvents();
